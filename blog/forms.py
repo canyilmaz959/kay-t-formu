30d708bd4e53ee206e7b24dc5c9af5e6eb8f800e
Can Yılmaz <muhammetcanyilmaz959@gmail.com>
2025-06-04 14:43:22 +0300

    projem

---
+++ blog/forms.py
@@ -0,0 +1,7 @@
+"""from django import forms
+from .models import Kullanici
+
+class KullaniciForm(forms.ModelForm):
+    class Meta:
+        model = Kullanici
+        fields = ['ad', 'soyad', 'email', 'sifre']"""
